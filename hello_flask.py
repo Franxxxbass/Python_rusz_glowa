5b2b45d89cf1be62baf211f0f1b83aad39fd4152
Franxxxbass <63103120+Franxxxbass@users.noreply.github.com>
2022-08-03 09:47:18 +0200

    added base.html

--- hello_flask.py
+++ hello_flask.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from vsearch import search4letters
 
 app = Flask(__name__)
 
@@ -6,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello() -> str:
     return 'FLASK, o-o, he\'s a miracle!'
+
+
+@app.route('/search4')
+def do_search() -> str:
+    return str(search4letters('Kokolisan potasu', ))
 
 
 app.run()
