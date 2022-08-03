2583f84b1137bd5c20f569ac4c7264058246156d
Franxxxbass <63103120+Franxxxbass@users.noreply.github.com>
2022-08-03 10:25:19 +0200

    added entry.html, results.html, renamed file

--- search4web.py
+++ search4web.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from vsearch import search4letters
 
 app = Flask(__name__)
@@ -12,6 +12,11 @@ def hello() -> str:
 @app.route('/search4')
 def do_search() -> str:
     return str(search4letters('Kokolisan potasu', ))
+
+
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title='Welcome to search4letters')
 
 
 app.run()
